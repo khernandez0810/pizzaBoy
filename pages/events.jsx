7f60b04f1119c969115b21ccc4fec8a784d7c8f1
Kevin <khernandez0810@gmail.com>
2023-07-31 17:25:05 -0600

    add text for events page

---
+++ pages/events.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from '../styles/Events.module.css';
+
+
+const Events = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.text}>
+    Coming Soon !!!
+      </div>
+    </div>
+  );
+};
+
+export default Events;
