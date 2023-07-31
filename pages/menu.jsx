db580bdedacaeea1674ed75dce4a01bdec50d2d2
Kevin <khernandez0810@gmail.com>
2023-07-31 17:12:43 -0600

    fix route issue

--- pages/menu.jsx
+++ pages/menu.jsx
@@ -31,7 +31,7 @@ export const getServerSideProps = async (ctx) => {
     admin = true
   }
 
-  const res = await axios.get("http://localhost:3000/api/products");
+  const res = await axios.get("https://pizza-boy.vercel.app/api/products");
   return {
     props: {
       pizzaList: res.data,
